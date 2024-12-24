45d504afb944a503259de1871d41330f29b93e4c
sonseongyeol <sonseong10@gmail.com>
2024-12-24 18:20:09 +0900

    vanillaExtractPlugin 설정 테스트

---
+++ src/styles/app.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  display: "block",
+  maxWidth: "520px",
+  margin: "0 auto",
+});
---
+++ src/styles/global.css.ts
@@ -0,0 +1,6 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("body", {
+  margin: 0,
+  padding: 0,
+});
