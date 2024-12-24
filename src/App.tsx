8f38d2fd902ef70f9cbb59e410f10cdf8c7ab519
sonseongyeol <sonseong10@gmail.com>
2024-12-24 15:36:07 +0900

    기상청 api 테스트및 검증

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,44 @@
 import { useEffect, useState } from "react";
-import { NaverApi } from "./utils/HTTP";
+import { WeatherApi } from "./utils/HTTP";
+import moment from "moment";
+import dfs_xy_conv from "./utils/position";
 
 function App() {
-  const [status, setLocation] = useState<null | string>(null);
+  const [status, setLocation] = useState<null | {
+    longitude: number;
+    latitude: number;
+  }>(null);
 
-  const temp = async () => {
-    await NaverApi.get(`/map-reversegeocode`, {
-      method: "GET",
-      params: {
-        coords: "126.73,37.54",
-        orders: "admcode",
-        output: "json",
-      },
-    }).then((json) => {
-      console.log(json);
-    });
-  };
-  useEffect(() => {
-    temp();
-  }, []);
+  // const initLoction = async () => {
+  //   if (status) {
+  //     await NaverApi.get(`/map-reversegeocode`, {
+  //       method: "GET",
+  //       params: {
+  //         coords: `${status?.longitude},${status?.latitude}`,
+  //         orders: "admcode",
+  //         output: "json",
+  //       },
+  //     }).then((json) => {
+  //       console.log(json);
+  //     });
+  //   }
+  // };
 
   function getLocation() {
     // Geolocation API 지원 여부 확인
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          const { latitude, longitude, accuracy } = position.coords;
-          setLocation(
-            `위도: ${latitude}, 경도: ${longitude}, 정확도: ${accuracy}미터`
-          );
+          const { latitude, longitude } = position.coords;
+          setLocation({
+            longitude,
+            latitude,
+          });
         },
         (error) => {
-          setLocation(`위치 정보를 가져올 수 없습니다: ${error.message}`);
-        },
+          alert(error);
+          setLocation(null);
+        }, //
         {
           enableHighAccuracy: true,
           timeout: 10000,
@@ -40,13 +46,102 @@ function App() {
         }
       );
     } else {
-      setLocation("브라우저가 위치 서비스를 지원하지 않습니다.");
+      setLocation(null);
     }
   }
 
+  const timePicker = () => {
+    const info: { time: undefined | string; date: undefined | string } = {
+      time: undefined,
+      date: undefined,
+    };
+    const currentTime = moment().format("HHmm");
+    info.date = moment().format("YYYYMMDD").toString();
+    if (moment(currentTime).isBetween("1200", "0215")) {
+      info.date = moment().subtract(1, "days").toString();
+      info.time = "2300";
+    } else if (moment(currentTime).isBetween("0215", "0515")) {
+      info.time = "0200";
+    } else if (moment(currentTime).isBetween("0515", "0815")) {
+      info.time = "0500";
+    } else if (moment(currentTime).isBetween("0815", "1115")) {
+      info.time = "0800";
+    } else if (moment(currentTime).isBetween("1115", "1415")) {
+      info.time = "1100";
+    } else if (moment(currentTime).isBetween("1415", "1715")) {
+      info.time = "1400";
+    } else if (moment(currentTime).isBetween("1715", "2015")) {
+      info.time = "1700";
+    } else if (moment(currentTime).isBetween("2015", "2315")) {
+      info.time = "2000";
+    } else {
+      info.time = "2300";
+    }
+
+    return info;
+  };
+
+  const timeWeather = async () => {
+    if (!status) {
+      return;
+    }
+
+    const info = timePicker();
+    const rs = dfs_xy_conv("toXY", status.latitude, status.longitude);
+    await WeatherApi.get(`/1360000/VilageFcstInfoService_2.0/getVilageFcst`, {
+      params: {
+        pageNo: 1,
+        numOfRows: 260,
+        dataType: "JSON",
+        base_date: info.date,
+        base_time: info.time,
+        nx: rs.x,
+        ny: rs.y,
+      },
+    }).then((json) => {
+      const temp = json.data.response.body.items.item;
+
+      //   .filter((e) => e);
+
+      console.log(temp);
+    });
+  };
+
+  const nowWeather = async () => {
+    if (!status) {
+      return;
+    }
+
+    const info = timePicker();
+    const rs = dfs_xy_conv("toXY", 37.5412874, 126.7376916);
+    await WeatherApi.get(`/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst`, {
+      params: {
+        pageNo: 1,
+        numOfRows: 60,
+        dataType: "JSON",
+        base_date: info.date,
+        base_time: info.time,
+        nx: rs.x,
+        ny: rs.y,
+      },
+    }).then((res) => console.log(res));
+  };
+
+  useEffect(() => {
+    getLocation();
+    timeWeather();
+  }, [status?.latitude]);
+
   return (
     <>
-      <p>{status}</p>
+      {status !== undefined ? (
+        <span>{status?.latitude + "," + status?.longitude}</span>
+      ) : (
+        <span>
+          현재 위치를 탐색 할 수 없습니다. 위치서비스를 활성화 하거나, 검색어를
+          입력해 주세요.
+        </span>
+      )}
       <button onClick={getLocation}></button>
     </>
   );
--- src/utils/HTTP.ts
+++ src/utils/HTTP.ts
@@ -4,9 +4,16 @@ const NaverApi = axios.create({
   baseURL: "/api",
   headers: {
     "Content-Type": "application.json",
-    "x-ncp-apigw-api-key-id": import.meta.env.VITE_APP_NAVER_CLIENT_ID ?? "",
-    "x-ncp-apigw-api-key": import.meta.env.VITE_APP_NAVER_CLIENT_SECRET ?? "",
+    "x-ncp-apigw-api-key-id": import.meta.env.VITE_APP_NAVER_CLIENT_ID || "",
+    "x-ncp-apigw-api-key": import.meta.env.VITE_APP_NAVER_CLIENT_SECRET || "",
   },
 });
 
-export { NaverApi };
+const WeatherApi = axios.create({
+  baseURL: "http://apis.data.go.kr",
+  params: {
+    serviceKey: import.meta.env.VITE_APP_WEATHER_KEY || "",
+  },
+});
+
+export { NaverApi, WeatherApi };
