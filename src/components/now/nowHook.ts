aa6ec2aa733f83510a10b7fa925762aa7b1881f6
sonseongyeol <sonseong10@gmail.com>
2025-01-03 16:01:40 +0900

    현재 시각 기준 초단기 날시정보로 변경

--- src/components/now/nowHook.ts
+++ src/components/now/nowHook.ts
@@ -3,37 +3,6 @@ import dfs_xy_conv from "../../utils/position";
 import { WeatherApi } from "../../utils/HTTP";
 import moment from "moment";
 
-const timePicker = () => {
-  const info: { time: undefined | string; date: undefined | string } = {
-    time: undefined,
-    date: undefined,
-  };
-  const currentTime = moment().format("HHmm");
-  info.date = moment().format("YYYYMMDD").toString();
-  if (moment(currentTime).isBetween("1200", "0215")) {
-    info.date = moment().subtract(1, "days").toString();
-    info.time = "2300";
-  } else if (moment(currentTime).isBetween("0215", "0515")) {
-    info.time = "0200";
-  } else if (moment(currentTime).isBetween("0515", "0815")) {
-    info.time = "0500";
-  } else if (moment(currentTime).isBetween("0815", "1115")) {
-    info.time = "0800";
-  } else if (moment(currentTime).isBetween("1115", "1415")) {
-    info.time = "1100";
-  } else if (moment(currentTime).isBetween("1415", "1715")) {
-    info.time = "1400";
-  } else if (moment(currentTime).isBetween("1715", "2015")) {
-    info.time = "1700";
-  } else if (moment(currentTime).isBetween("2015", "2315")) {
-    info.time = "2000";
-  } else {
-    info.time = "2400";
-  }
-
-  return info;
-};
-
 const nowWeatherAPI = async (status?: {
   latitude: number;
   longitude: number;
@@ -42,15 +11,14 @@ const nowWeatherAPI = async (status?: {
     return;
   }
 
-  const info = timePicker();
   const rs = dfs_xy_conv("toXY", status.latitude, status.longitude);
 
   const params = {
     pageNo: 1,
     numOfRows: 60,
     dataType: "JSON",
-    base_date: info.date,
-    base_time: info.time,
+    base_date: moment().format("YYYYMMDD"),
+    base_time: moment().format("HHmm"),
     nx: rs.x,
     ny: rs.y,
   };
