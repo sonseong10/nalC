35c1b891ac360d4cced0ffe20fbe6904238b397a
sonseongyeol <sonseong10@gmail.com>
2025-04-03 13:54:47 +0900

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from './App.tsx'
 import "./styles/global.css.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
