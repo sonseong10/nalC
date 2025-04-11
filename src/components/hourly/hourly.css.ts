34f197490cdd581290d78748d4fab5ada2f16e00
sonseongyeol <sonseong10@gmail.com>
2025-04-11 15:41:01 +0900

    Markup of components by hourly

---
+++ src/components/hourly/hourly.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+const card = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  flex: "0 0 100px",
+});
+
+export { card };
--- src/components/hourly/hourly.tsx
+++ src/components/hourly/hourly.tsx
@@ -1,7 +1,9 @@
 import moment from "moment";
+import { flexRow } from "../../styles/app.css";
 import { WeatherApi } from "../../utils/HTTP";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import dfs_xy_conv from "../../utils/position";
+import { card } from "./hourly.css";
 
 function adjustMinutes(time: moment.Moment): string {
   const minutes = time.minutes();
@@ -26,7 +28,7 @@ const timePicker = () => {
   return info;
 };
 
-const nowWeatherAPI = async (status?: {
+const todayWeatherAPI = async (status?: {
   latitude: number;
   longitude: number;
 }) => {
@@ -39,7 +41,7 @@ const nowWeatherAPI = async (status?: {
   const rs = dfs_xy_conv("toXY", status.latitude, status.longitude);
   const params = {
     pageNo: 1,
-    numOfRows: 229,
+    numOfRows: 288,
     dataType: "JSON",
     base_date: date,
     base_time: "1400",
@@ -54,9 +56,23 @@ const nowWeatherAPI = async (status?: {
     }
   )
     .then((res) => {
-      console.log(res.data.response.body?.items?.item);
+      const grouped: Array<
+        {
+          baseDate: string;
+          baseTime: string;
+          category: string;
+          fcstDate: string;
+          fcstTime: string;
+          fcstValue: string;
+          nx: number;
+          ny: number;
+        }[]
+      > = [];
+      for (let i = 0; i < res.data.response.body?.items?.item.length; i += 12) {
+        grouped.push(res.data.response.body?.items?.item.slice(i, i + 12));
+      }
 
-      return [];
+      return grouped;
     })
     .catch((error) => {
       console.error(error);
@@ -66,23 +82,29 @@ const nowWeatherAPI = async (status?: {
   return data;
 };
 
-const useNowWeatherInfo = (status?: {
+const useTodayWeatherInfo = (status?: {
   latitude: number;
   longitude: number;
 }) => {
   const [toDayInfo, setToDayInfo] = useState<
-    Array<{
-      category: string;
-      fcstValue: string | number;
-      baseDate: string;
-      baseTime: string;
-    }>
+    Array<
+      {
+        baseDate: string;
+        baseTime: string;
+        category: string;
+        fcstDate: string;
+        fcstTime: string;
+        fcstValue: string;
+        nx: number;
+        ny: number;
+      }[]
+    >
   >([]);
 
   useEffect(() => {
     if (status) {
       const fetchAndSetData = async () => {
-        const data = await nowWeatherAPI(status);
+        const data = await todayWeatherAPI(status);
         if (data) {
           setToDayInfo(data);
         }
@@ -102,12 +124,53 @@ interface NowWeatherProps {
 }
 
 function Hourly({ status }: NowWeatherProps) {
-  useNowWeatherInfo(status);
+  const { toDayInfo } = useTodayWeatherInfo(status);
+
+  console.log(toDayInfo);
+
   return (
     <>
       <h2>시간별 날씨</h2>
 
-      <div></div>
+      <div className={flexRow}>
+        {toDayInfo.map((info, index) => (
+          <div key={index} className={card}>
+            <span>{info[0].fcstTime.substring(0, 2)}시</span>
+            {info.map(({ category, fcstValue }, index) => {
+              return (
+                <Fragment key={index}>
+                  {category === "TMP" && <strong>{fcstValue}°</strong>}
+
+                  {category === "SKY" && (
+                    <img
+                      src="https://ssl.pstatic.net/static/weather/image/icon_weather/ico_animation_wt2.svg"
+                      alt="맑음"
+                    />
+                  )}
+                  {category === "POP" && (
+                    <span>
+                      {Number(fcstValue) <= 0 ? "-" : `${fcstValue}%`}
+                    </span>
+                  )}
+                  {category === "REH" && <span>{fcstValue}</span>}
+                </Fragment>
+              );
+            })}
+
+            {info.map(
+              ({ category, fcstValue }, index) =>
+                (category === "WSD" || category === "VEC") && (
+                  <div key={index}>
+                    <span>{category === "VEC" && fcstValue}</span>
+                    <span>
+                      {category === "WSD" && Math.round(Number(fcstValue))}
+                    </span>
+                  </div>
+                )
+            )}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
