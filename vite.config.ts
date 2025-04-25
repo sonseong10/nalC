413cc0048db4152f65836a7604552f43412e1157
sonseongyeol <sonseong10@gmail.com>
2025-04-25 19:16:00 +0900

    Update vite.config.ts to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react()],
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-  },
-  server: {
-    proxy: {
-      "/api/map-reversegeocode": {
-        target: "https://naveropenapi.apigw.ntruss.com",
-        changeOrigin: true,
-        rewrite: (path) =>
-          path.replace(
-            /^\/api\/map-reversegeocode/,
-            "/map-reversegeocode/v2/gc"
-          ),
-        secure: false,
-        ws: true,
+  return {
+    base: "/nalC/",
+    plugins: [vanillaExtractPlugin(), react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_ENV),
+      "process.env.NODE_ENV": JSON.stringify(mode),
+    },
+    server: {
+      proxy: {
+        "/api/map-reversegeocode": {
+          target: "https://naveropenapi.apigw.ntruss.com",
+          changeOrigin: true,
+          rewrite: (path) =>
+            path.replace(
+              /^\/api\/map-reversegeocode/,
+              "/map-reversegeocode/v2/gc"
+            ),
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
+  };
 });
