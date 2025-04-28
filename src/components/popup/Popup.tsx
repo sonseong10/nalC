c2ddeb498c7efb8eb845d4c2d16e708cb18c84b4
sonseongyeol <sonseong10@gmail.com>
2025-04-28 18:33:34 +0900

    Add kakao geo address API and style Popup components

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -1,11 +1,83 @@
-import { popup, popupHeader, popupTitle } from "./popup.css";
+import { useRef, useState } from "react";
+import {
+  addressGroup,
+  addressItem,
+  addressName,
+  closedButton,
+  dataNone,
+  deleteButton,
+  itemButton,
+  placeName,
+  popup,
+  popupHeader,
+  popupTitle,
+  searchForm,
+  searchInput,
+  sessionTitle,
+} from "./popup.css";
+import { KakaoApi } from "../../utils/HTTP";
 
 interface IPopupProps {
   isShow: boolean;
   change: (state?: boolean) => void;
 }
 
+interface IAddressData {
+  address_name: string;
+  category_group_code: string;
+  category_group_name: string;
+  category_name: string;
+  distance: string;
+  id: string;
+  phone: string;
+  place_name: string;
+  place_url: string;
+  road_address_name: string;
+  x: string;
+  y: string;
+}
+
+interface IResKakaoKeywordAddress {
+  documents: IAddressData[];
+  meta: {
+    is_end: boolean;
+    pageable_count: number;
+    same_name: { keyword: ""; region: object[]; selected_region: string };
+    total_count: number;
+  };
+}
+
 function Popup({ isShow, change }: IPopupProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [address, setAddress] = useState<Array<IAddressData> | undefined>(
+    undefined
+  );
+  const [totalCount, setTotalCount] = useState(0);
+  const getAddress = async () => {
+    if (inputRef.current && inputRef.current.value.trim().length <= 0) {
+      return;
+    } else {
+      await KakaoApi.get<IResKakaoKeywordAddress>("/v2/local/search/keyword", {
+        params: {
+          query: inputRef.current?.value,
+        },
+      }).then((res) => {
+        setAddress(res?.data?.documents);
+        setTotalCount(res?.data?.meta?.total_count);
+      });
+    }
+  };
+
+  function colorText(inputText: string) {
+    const regex = new RegExp(inputRef.current!.value, "g");
+    const result = inputText.replace(
+      regex,
+      `<span style="color: #0066ff;">${inputRef.current!.value}</span>`
+    );
+
+    return { __html: result };
+  }
+
   return (
     <>
       {!isShow ? (
@@ -20,44 +92,70 @@ function Popup({ isShow, change }: IPopupProps) {
                 e.preventDefault();
                 change(false);
               }}
+              className={closedButton}
             >
               <span>닫기</span>
             </button>
           </header>
           <div>
-            <div>
-              <input placeholder="행정동 입력" type="text" />
-            </div>
+            <form
+              action={void 0}
+              method="get"
+              onSubmit={(e) => {
+                e.preventDefault();
+                getAddress();
+              }}
+              className={searchForm}
+            >
+              <input
+                id="search"
+                placeholder="행정동 입력"
+                type="text"
+                className={searchInput}
+                ref={inputRef}
+              />
+            </form>
 
             <div>
-              <p>총 1건이 검색되었습니다.</p>
-              <ol>
-                <li>
-                  <div>
-                    <div>
-                      <button>추가</button>
-                      <strong>인천광역시 xxx구 xxx동</strong>
-                    </div>
-                    <div>
-                      <button>삭제</button>
-                    </div>
+              <span className={sessionTitle}>
+                {totalCount <= 0
+                  ? "검색결과"
+                  : `총 ${totalCount.toLocaleString()}건이 검색되었습니다.`}
+              </span>
+              <ol className={addressGroup}>
+                {!address || address.length <= 0 ? (
+                  <div className={dataNone}>
+                    <span>검색결과가 없습니다.</span>
+                    <span>검색어를 확인 해주세요</span>
                   </div>
-                </li>
+                ) : (
+                  address.map((info, index) => (
+                    <li key={index} className={addressItem}>
+                      <button className={itemButton}>
+                        <span className={placeName}>{info.place_name}</span>
+                        <strong
+                          className={addressName}
+                          dangerouslySetInnerHTML={colorText(info.address_name)}
+                        ></strong>
+                      </button>
+                    </li>
+                  ))
+                )}
               </ol>
             </div>
 
             <div>
-              <p>관심목록</p>
-              <ol>
-                <li>
+              <span className={sessionTitle}>관심목록(1/7)</span>
+              <ol className={addressGroup}>
+                <li className={addressItem}>
+                  <button className={itemButton}>
+                    <span className={placeName}>{""}</span>
+                    <strong className={addressName}>
+                      {"인천 xxx구 xxx동"}
+                    </strong>
+                  </button>
                   <div>
-                    <div>
-                      <button>추가</button>
-                      <strong>인천광역시 xxx구 xxx동</strong>
-                    </div>
-                    <div>
-                      <button>삭제</button>
-                    </div>
+                    <button className={deleteButton}>X</button>
                   </div>
                 </li>
               </ol>
--- src/components/popup/popup.css.ts
+++ src/components/popup/popup.css.ts
@@ -1,4 +1,5 @@
 import { style } from "@vanilla-extract/css";
+import SVG from "../../styles/icon";
 
 const popup = style({
   position: "fixed",
@@ -24,6 +25,133 @@ const popupHeader = style({
 
 const popupTitle = style({
   margin: 0,
+  fontSize: "14px",
 });
 
-export { popup, popupHeader, popupTitle };
+const closedButton = style({
+  backgroundColor: "#fff",
+  border: "none",
+  cursor: "pointer",
+
+  ":hover": {
+    color: "#ff4949",
+  },
+});
+
+const searchForm = style({
+  position: "relative",
+  margin: "12px 10px",
+
+  "::before": {
+    position: "absolute",
+    top: "50%",
+    left: "8px",
+    display: "block",
+    width: "24px",
+    height: "24px",
+    background: `url(${SVG.Search("#1d1d1d")}) no-repeat center center`,
+    transform: "translateY(-50%)",
+    zIndex: "1",
+    content: "",
+  },
+});
+
+const searchInput = style({
+  position: "relative",
+  width: "100%",
+  height: "40px",
+  paddingLeft: "36px",
+  border: "1px solid #ddd",
+  fontSize: "16px",
+  borderRadius: "24px",
+});
+
+const sessionTitle = style({
+  display: "inline-block",
+  padding: "0 16px",
+  margin: "8px 0",
+  fontSize: "13px",
+  color: "#828282",
+});
+
+const addressGroup = style({
+  listStyle: "none",
+  margin: 0,
+  padding: 0,
+  maxHeight: "240px",
+  overflowY: "auto",
+});
+
+const addressItem = style({
+  display: "flex",
+  alignItems: "center",
+  padding: "0 16px",
+  borderBottom: "1px solid #ddd",
+
+  selectors: {
+    "&:first-child": {
+      borderTop: "1px solid #ddd",
+    },
+  },
+});
+
+const itemButton = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center",
+  width: "100%",
+  height: "56px",
+  border: 0,
+  padding: 0,
+  backgroundColor: "#fff",
+  fontSize: "14px",
+  flex: "1",
+  cursor: "pointer",
+
+  ":hover": {
+    opacity: 0.5,
+  },
+});
+
+const placeName = style({
+  marginBottom: "4px",
+});
+
+const addressName = style({
+  color: "#828282",
+});
+
+const deleteButton = style({
+  height: "56px",
+  border: "none",
+  backgroundColor: `#fff`,
+  cursor: "pointer",
+
+  ":hover": {
+    color: "#ff4949",
+  },
+});
+
+const dataNone = style({
+  padding: "16px",
+  textAlign: "center",
+  color: "gray",
+});
+
+export {
+  popup,
+  popupHeader,
+  popupTitle,
+  closedButton,
+  searchForm,
+  searchInput,
+  sessionTitle,
+  addressGroup,
+  addressItem,
+  itemButton,
+  placeName,
+  addressName,
+  deleteButton,
+  dataNone,
+};
